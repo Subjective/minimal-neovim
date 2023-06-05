fc2de295653585c4b2e4c4700756c5cfe8265b27
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-05 14:23:08 -0700

    feat(telescope): invert sorting direction and increase display width

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,15 @@ return {
     },
     opts = {
       defaults = {
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
         mappings = {
           i = {
             ["<C-n>"] = "cycle_history_next",
