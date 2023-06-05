a27812e1f4081c64e7e6ea13cf3bfb55f40fa1e3
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-05 14:48:14 -0700

    chore(keymaps): reorganize keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,20 @@
+-- Standard operations
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>")
+vim.keymap.set("n", "<leader>c", "<cmd>confirm bdelete<cr>")
+vim.keymap.set("n", "<leader>n", "<cmd>enew<cr>")
+
 -- Move around splits
 vim.keymap.set("n", "<C-h>", "<C-w>h", {})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+
+-- Packages
+vim.keymap.set("n", "<leader>ps", function() require("lazy").home() end)
+vim.keymap.set("n", "<leader>pS", function() require("lazy").sync() end)
+vim.keymap.set("n", "<leader>pm", "<cmd>Mason<cr>")
+vim.keymap.set("n", "<leader>pM", "<cmd>MasonUpdateAll<cr>")
 
 -- Telescope
 local builtin = require "telescope.builtin"
@@ -20,18 +32,6 @@ vim.keymap.set("n", "<leader>/",
   function() require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1) end)
 vim.keymap.set("v", "<leader>/",
   "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>")
-
--- Packages
-vim.keymap.set("n", "<leader>ps", function() require("lazy").home() end)
-vim.keymap.set("n", "<leader>pS", function() require("lazy").sync() end)
-vim.keymap.set("n", "<leader>pm", "<cmd>Mason<cr>")
-vim.keymap.set("n", "<leader>pM", "<cmd>MasonUpdateAll<cr>")
-
--- Standard operations
-vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
-vim.keymap.set("n", "<leader>q", "<cmd>confirm q<cr>")
-vim.keymap.set("n", "<leader>c", "<cmd>confirm bdelete<cr>")
-vim.keymap.set("n", "<leader>n", "<cmd>enew<cr>")
 
 -- Undotree
 vim.keymap.set("n", "<leader>U", "<cmd>UndotreeToggle<cr>")
