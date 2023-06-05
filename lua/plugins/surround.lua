73c83d607472bee3590334997e85143e2a7d333b
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-05 14:40:48 -0700

    fix(surround): keybindings conflict with leap.nvim

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,9 +2,18 @@ return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
-    })
-  end
+  opts = {
+    keymaps = {
+      insert = "<C-g>s",
+      insert_line = "<C-g>Z",
+      normal = "ys",
+      normal_cur = "yss",
+      normal_line = "yZ",
+      normal_cur_line = "yZZ",
+      visual = "Z",
+      visual_line = "gZ",
+      delete = "ds",
+      change = "cs",
+    },
+  }
 }
