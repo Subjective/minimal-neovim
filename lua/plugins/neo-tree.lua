f38d85a8d1227680deca2a5516f0fe9df8be9c53
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-18 02:36:22 -0700

    fix(neo-tree): correctly display git status icons

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,9 +13,26 @@ return {
       width = 30,
     },
     filesystem = {
-      follow_current_file = true,
-      hijack_netrw_behavior = "open_current",
+      follow_current_file    = true,
+      hijack_netrw_behavior  = "open_current",
       use_libuv_file_watcher = true,
     },
-  }
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added     = "",  -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified  = "",  -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted   = "✖", -- this can only be used in the git_status source
+          renamed   = "", -- this can only be used in the git_status source
+          -- Status type
+          untracked = "",
+          ignored   = "",
+          unstaged  = "",
+          staged    = "",
+          conflict  = "",
+        }
+      }
+    }
+  },
 }
