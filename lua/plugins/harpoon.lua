77f571b4f749ec03f0f4bfa7816878ceddaceb60
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-19 04:31:15 -0700

    feat(harpoon): add mapping to navigate between files

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -12,6 +12,10 @@ return {
     { prefix .. "e", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle quick menu" },
     { "<C-p>",       function() require("harpoon.ui").nav_prev() end,          desc = "Goto previous mark" },
     { "<C-n>",       function() require("harpoon.ui").nav_next() end,          desc = "Goto next mark" },
+    { "<leader>1",   function() require("harpoon.ui").nav_file(1) end,         desc = "Goto file 1" },
+    { "<leader>2",   function() require("harpoon.ui").nav_file(2) end,         desc = "Goto file 2" },
+    { "<leader>3",   function() require("harpoon.ui").nav_file(3) end,         desc = "Goto file 3" },
+    { "<leader>4",   function() require("harpoon.ui").nav_file(4) end,         desc = "Goto file 4" },
     { prefix .. "m", "<cmd>Telescope harpoon marks<CR>",                       desc = "Show marks in Telescope" },
     {
       prefix .. "t",
