13310ffe83d480af0c9a7c61496894b04d858a49
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-18 17:16:47 -0700

    feat(lsp): show diagnostics icons in sign column

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -101,6 +101,13 @@ return {
 
       lsp.on_attach(function(client, bufnr) lsp.default_keymaps { buffer = bufnr } end)
 
+      lsp.set_sign_icons({
+        error = '',
+        warn = '',
+        hint = '󰌵',
+        info = '󰋼'
+      })
+
       -- (Optional) Configure lua language server for neovim
       require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
