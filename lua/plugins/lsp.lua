f8107f434490315cd561fe8cf45b4892eb4c022a
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-18 19:48:22 -0700

    fix(cmp): `has_words_before` function not defined

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,6 +36,11 @@ return {
       local luasnip = require "luasnip"
       local cmp_action = require("lsp-zero.cmp").action()
 
+      local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
       cmp.setup {
         mapping = {
           ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
