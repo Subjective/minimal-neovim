90907e65262fe60e059952c50af9a12b54cf3a7e
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-06-20 18:39:05 -0700

    fix(surround): nvim-surround default keymaps

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -5,11 +5,11 @@ return {
   opts = {
     keymaps = {
       insert = "<C-g>s",
-      insert_line = "<C-g>Z",
+      insert_line = "<C-g>S",
       normal = "ys",
       normal_cur = "yss",
-      normal_line = "yZ",
-      normal_cur_line = "yZZ",
+      normal_line = "ys",
+      normal_cur_line = "ySS",
       visual = "Z",
       visual_line = "gZ",
       delete = "ds",
