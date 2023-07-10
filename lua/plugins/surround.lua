07e4c17110867b25cb89ec0e627377a84cdc202c
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-10 00:24:50 -0700

    fix(surround): resolve conflict with leap keybinds

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -6,14 +6,15 @@ return {
     keymaps = {
       insert = "<C-g>s",
       insert_line = "<C-g>S",
-      normal = "ys",
-      normal_cur = "yss",
-      normal_line = "ys",
-      normal_cur_line = "ySS",
-      visual = "Z",
-      visual_line = "gZ",
-      delete = "ds",
-      change = "cs",
+      normal = "gs",
+      normal_cur = "gss",
+      normal_line = "gS",
+      normal_cur_line = "gSS",
+      visual = "gs",
+      visual_line = "gS",
+      delete = "gsd",
+      change = "gsr",
+      change_line = "gSr",
     },
   }
 }
