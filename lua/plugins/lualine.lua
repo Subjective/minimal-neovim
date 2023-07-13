dd89feb291524d5fba1d8ae68d25319db5e4031a
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-13 16:43:14 -0700

    feat(lualine): add grapple integration to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,12 +2,21 @@ return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
   dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-  config = function()
-    require("lualine").setup {
-      options = {
-        theme = "catppuccin",
-        -- ... the rest of your lualine config
+  opts = {
+    options = {
+      theme = "catppuccin",
+    },
+    sections = {
+      lualine_c = {
+        {
+          function()
+            local key = require("grapple").key()
+            return " " .. key
+          end,
+          cond = require("grapple").exists,
+        },
+        'filename'
       },
-    }
-  end,
+    },
+  }
 }
