6f72553e46f8148f5ac1cae28c1bc9a0b53fffb4
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-13 05:38:24 -0700

    feat(leap): add flit.nvim for enhanced f/t motions

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,30 +1,46 @@
 return {
-  "ggandor/leap.nvim",
-  dependencies = {
-    "tpope/vim-repeat",
+  {
+    "ggandor/leap.nvim",
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    keys = {
+      { "s",  "<Plug>(leap-forward-to)",    mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S",  "<Plug>(leap-backward-to)",   mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "x",  "<Plug>(leap-forward-till)",  mode = { "x", "o" },      desc = "Leap forward till" },
+      { "X",  "<Plug>(leap-backward-till)", mode = { "x", "o" },      desc = "Leap backward till" },
+      { "zS", "<Plug>(leap-from-window)",   mode = { "n", "x", "o" }, desc = "Leap from window" },
+    },
+    opts = function()
+      require("leap").add_repeat_mappings(";", ",", {
+        relative_directions = true,
+        modes = { "n", "x", "o" },
+      })
+      return {
+        highlight_unlabeled_phase_one_targets = true,
+        special_keys = {
+          next_target = ";",
+          prev_target = ",",
+          next_group = "<space>",
+          prev_group = "<tab>",
+          multi_accept = "<enter>",
+          multi_revert = "<backspace>",
+        },
+      }
+    end,
   },
-  keys = {
-    { "s",  "<Plug>(leap-forward-to)",    mode = { "n", "x", "o" }, desc = "Leap forward to" },
-    { "S",  "<Plug>(leap-backward-to)",   mode = { "n", "x", "o" }, desc = "Leap backward to" },
-    { "x",  "<Plug>(leap-forward-till)",  mode = { "x", "o" },      desc = "Leap forward till" },
-    { "X",  "<Plug>(leap-backward-till)", mode = { "x", "o" },      desc = "Leap backward till" },
-    { "zS", "<Plug>(leap-from-window)",   mode = { "n", "x", "o" }, desc = "Leap from window" },
+  {
+    "ggandor/flit.nvim",
+    dependencies = { "ggandor/leap.nvim" },
+    keys = {
+      { "f", mode = { "n", "x", "o" }, desc = "Flit forward to" },
+      { "F", mode = { "n", "x", "o" }, desc = "Flit backward to" },
+      { "t", mode = { "n", "x", "o" }, desc = "Flit forward till" },
+      { "T", mode = { "n", "x", "o" }, desc = "Flit backward till" },
+    },
+    opts = {
+      labeled_modes = "nxo",
+      multiline = false,
+    },
   },
-  opts = function()
-    require("leap").add_repeat_mappings(";", ",", {
-      relative_directions = true,
-      modes = { "n", "x", "o" },
-    })
-    return {
-      highlight_unlabeled_phase_one_targets = true,
-      special_keys = {
-        next_target = ";",
-        prev_target = ",",
-        next_group = "<space>",
-        prev_group = "<tab>",
-        multi_accept = "<enter>",
-        multi_revert = "<backspace>",
-      },
-    }
-  end,
 }
