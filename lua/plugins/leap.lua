698740e845da23c34917d6f0960c8e04c4946169
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-13 05:22:54 -0700

    fix(leap): improve keybinds

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,18 +1,30 @@
 return {
   "ggandor/leap.nvim",
-  keys = {
-    { "s",  mode = { "n", "x", "o" }, desc = "Leap forward to" },
-    { "S",  mode = { "n", "x", "o" }, desc = "Leap backward to" },
-    { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
-  },
-  config = function(_, opts)
-    local leap = require "leap"
-    for k, v in pairs(opts) do
-      leap.opts[k] = v
-    end
-    leap.add_default_mappings(true)
-  end,
   dependencies = {
     "tpope/vim-repeat",
   },
+  keys = {
+    { "s",  "<Plug>(leap-forward-to)",    mode = { "n", "x", "o" }, desc = "Leap forward to" },
+    { "S",  "<Plug>(leap-backward-to)",   mode = { "n", "x", "o" }, desc = "Leap backward to" },
+    { "x",  "<Plug>(leap-forward-till)",  mode = { "x", "o" },      desc = "Leap forward till" },
+    { "X",  "<Plug>(leap-backward-till)", mode = { "x", "o" },      desc = "Leap backward till" },
+    { "zS", "<Plug>(leap-from-window)",   mode = { "n", "x", "o" }, desc = "Leap from window" },
+  },
+  opts = function()
+    require("leap").add_repeat_mappings(";", ",", {
+      relative_directions = true,
+      modes = { "n", "x", "o" },
+    })
+    return {
+      highlight_unlabeled_phase_one_targets = true,
+      special_keys = {
+        next_target = ";",
+        prev_target = ",",
+        next_group = "<space>",
+        prev_group = "<tab>",
+        multi_accept = "<enter>",
+        multi_revert = "<backspace>",
+      },
+    }
+  end,
 }
