7e8ed723fee23dcc57566dc08aa25f07ed086f42
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-07-21 21:07:23 -0700

    feat(surround): simplify visual mode bindings

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -2,5 +2,20 @@ return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
   event = "VeryLazy",
-  opts = {}
+  opts = {
+    keymaps = {
+      insert = "<C-g>s",
+      insert_line = "<C-g>S",
+      normal = "ys",
+      normal_cur = "yss",
+      normal_line = "yS",
+      normal_cur_line = "ySS",
+      visual = "s",
+      visual_line = "S",
+      delete = "ds",
+      change = "cs",
+      change_line = "cS",
+    },
+
+  }
 }
