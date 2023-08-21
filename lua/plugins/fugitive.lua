3266a44e2fc51ce8d905434507e2016e103dd97c
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-08-21 14:30:39 -0700

    feat(fugitive): make q quit fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,12 @@
 return {
   "tpope/vim-fugitive",
-  event = "VeryLazy"
+  event = "VeryLazy",
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Make q close fugitive",
+      group = vim.api.nvim_create_augroup("quit_fugitive", { clear = true }),
+      pattern = { "fugitive", "fugitiveblame" },
+      callback = function() vim.keymap.set("n", "q", "<Plug>fugitive:gq", { buffer = true }) end,
+    })
+  end,
 }
