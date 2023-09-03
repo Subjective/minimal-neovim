7fc552e28455cbc31136613287d0b1e321c7883e
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-09-02 17:38:54 -0700

    feat(neo-tree): add "h"/"l" bindings for navigation

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,13 +9,6 @@ return {
   },
   opts = {
     close_if_last_window = true,
-    window = {
-      width = 30,
-      fuzzy_finder_mappings = {
-        ["<C-j>"] = "move_cursor_down",
-        ["<C-k>"] = "move_cursor_up",
-      },
-    },
     filesystem = {
       follow_current_file    = true,
       hijack_netrw_behavior  = "open_current",
@@ -37,6 +30,39 @@ return {
           conflict  = "",
         }
       }
-    }
+    },
+    commands = {
+      parent_or_close = function(state)
+        local node = state.tree:get_node()
+        if (node.type == "directory" or node:has_children()) and node:is_expanded() then
+          state.commands.toggle_node(state)
+        else
+          require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+        end
+      end,
+      child_or_open = function(state)
+        local node = state.tree:get_node()
+        if node.type == "directory" or node:has_children() then
+          if not node:is_expanded() then -- if unexpanded, expand
+            state.commands.toggle_node(state)
+          else                           -- if expanded and has children, seleect the next child
+            require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+          end
+        else -- if not a directory just open it
+          state.commands.open(state)
+        end
+      end,
+    },
+    window = {
+      width = 30,
+      mappings = {
+        h = "parent_or_close",
+        l = "child_or_open",
+      },
+      fuzzy_finder_mappings = {
+        ["<C-j>"] = "move_cursor_down",
+        ["<C-k>"] = "move_cursor_up",
+      },
+    },
   },
 }
