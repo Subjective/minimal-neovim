d9ecf8a29368ab75f69bdda63396b9232b40b098
Joshua Yin <56745535+Subjective@users.noreply.github.com>
2023-09-10 11:31:26 -0700

    Revert "feat(lualine): use flat lualine separators"
    This reverts commit 3a67d4501d16548484b19bee64724978b8b6efad.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,69 +1,3 @@
-local window_width_limit = 100
-
-local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand "%:t") ~= 1
-  end,
-  hide_in_width = function()
-    return vim.o.columns > window_width_limit
-  end,
-}
-
-local components = {
-  lsp = {
-    function()
-      local buf_clients = vim.lsp.get_clients { bufnr = 0 }
-      if #buf_clients == 0 then
-        return "LSP Inactive"
-      end
-
-      local buf_ft = vim.bo.filetype
-      local buf_client_names = {}
-      local copilot_active = false
-
-      -- add client
-      for _, client in pairs(buf_clients) do
-        if client.name ~= "null-ls" and client.name ~= "copilot" then
-          table.insert(buf_client_names, client.name)
-        end
-
-        if client.name == "copilot" then
-          copilot_active = true
-        end
-      end
-
-      -- -- add formatter
-      -- local formatters = require "lvim.lsp.null-ls.formatters"
-      -- local supported_formatters = formatters.list_registered(buf_ft)
-      -- vim.list_extend(buf_client_names, supported_formatters)
-      --
-      -- -- add linter
-      -- local linters = require "lvim.lsp.null-ls.linters"
-      -- local supported_linters = linters.list_registered(buf_ft)
-      -- vim.list_extend(buf_client_names, supported_linters)
-
-      local unique_client_names = table.concat(buf_client_names, ", ")
-      local language_servers = string.format("[%s]", unique_client_names)
-
-      if copilot_active then
-        language_servers = language_servers .. "%#SLCopilot#" .. " " .. "" .. "%*"
-      end
-
-      return language_servers
-    end,
-    color = { gui = "bold" },
-    cond = conditions.hide_in_width,
-  },
-  location = { "location" },
-  progress = {
-    "progress",
-    fmt = function()
-      return "%P/%L"
-    end,
-    color = {},
-  },
-}
-
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
@@ -71,12 +5,8 @@ return {
   opts = {
     options = {
       theme = "catppuccin",
-      section_separators = '',
-      component_separators = '',
     },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch' },
       lualine_c = {
         {
           function()
@@ -85,20 +15,8 @@ return {
           end,
           cond = require("grapple").exists,
         },
-        'filename',
-        'diff',
+        'filename'
       },
-      lualine_x = {
-        "diagnostics",
-        components.lsp,
-        "filetype"
-      },
-      lualine_y = {
-        components.location
-      },
-      lualine_z = {
-        components.progress
-      }
     },
     extensions = { "neo-tree", "lazy", "fugitive" },
   }
